f7339956ef8a32fa9a671201ef92e69f6dc2cef8
Dhaya3132 <dhayalanu103@gmail.com>
2024-04-08 08:28:27 +0530

    adding the data to button

--- dist/PopWrapper.js
+++ dist/PopWrapper.js
@@ -1,9 +1,14 @@
 const Buttons = document.querySelector('#Button');
 const List = document.querySelector('.DataContainer');
 const OuterContainer = document.querySelector('.OuterContainer');
+const close = document.querySelector('.close');
 
 Buttons.addEventListener('click', ()=>{
    List.style.display='block';
    OuterContainer.style.display='none';
-   
+});
+
+close.addEventListener('click', ()=>{
+   List.style.display = 'none';
+   OuterContainer.style.display='flex';
 });
